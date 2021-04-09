a484b2b36c8decfa45cabcfe12eae72d567b2541
SDS <SDS@SDS>
2021-04-09 20:12:20 +0300

    initial commit

---
+++ practise/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace practise
+{
+    class SayHello
+    {
+        static void Main(string[] args)
+        {
+            string say = "Hello world!";
+            Console.WriteLine(say);
+        }
+    }
+}
