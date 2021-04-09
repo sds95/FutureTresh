5aa729b2e41153b594969255d8bfe4bf3d173ff7
SlavikusVOG <slavaolish@gmail.com>
2021-04-09 20:29:29 +0300

    Slava created class1 with method that print into console Hello World.

---
+++ practice2/Class1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace practice2
+{
+    public class Class1
+    {
+        public void WriteHello()
+        {
+            Console.WriteLine("Hello world");
+        }
+    }
+}
